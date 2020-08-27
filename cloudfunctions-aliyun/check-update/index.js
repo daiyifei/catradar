34f8e5526892eb441c5df3826d76f027ca08c7e4
daiyifei <daiyifei@NUC.local>
2020-08-28 01:15:57 +0800

    新增热更新功能

---
+++ cloudfunctions-aliyun/check-update/index.js
@@ -0,0 +1,13 @@
+'use strict';
+const db = uniCloud.database()
+
+exports.main = async (event, context) => {
+	const { version: oldVer } = event.params
+	const { data: [{ version, note, url }] } = await db.collection('uni-app-version').get()
+	return {
+		update: parseInt(version.replace(/\./g,'')) > parseInt(oldVer.replace(/\./g,'')),
+		version,
+		note,
+		url
+	}
+}
