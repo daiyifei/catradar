f1fd130ad06d6eb9095f5be9a9b98c91e83dff32
daiyifei <daiyifei@NUC.local>
2020-08-28 01:15:57 +0800

    新增热更新功能

--- App.vue
+++ App.vue
@@ -2,6 +2,7 @@
 	import Vue from 'vue'
 	export default {
 		onLaunch: function() {
+			// custom bar
 			uni.getSystemInfo({
 				success: function(e) {
 					// #ifndef MP
@@ -12,25 +13,73 @@
 						Vue.prototype.CustomBar = e.statusBarHeight + 45;
 					};
 					// #endif
-			
+
 					// #ifdef MP-WEIXIN
 					Vue.prototype.StatusBar = e.statusBarHeight;
 					let custom = wx.getMenuButtonBoundingClientRect();
 					Vue.prototype.Custom = custom;
 					Vue.prototype.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
 					// #endif		
-			
+
 					// #ifdef MP-ALIPAY
 					Vue.prototype.StatusBar = e.statusBarHeight;
 					Vue.prototype.CustomBar = e.statusBarHeight + e.titleBarHeight;
 					// #endif
 				},
 			})
+
+			// check update
+			// #ifdef APP-PLUS
+			plus.runtime.getProperty(plus.runtime.appid, ({
+				version
+			}) => {
+				this.$request('check-update', '', {
+					version
+				}).then(res => {
+					if (res.update) {
+						uni.showModal({
+							title: '检测到新版本',
+							content: res.version + '\n' + res.note,
+							confirmText: '立即更新',
+							cancelText: '以后再说',
+							success: ({ confirm }) => {
+								if (confirm) {
+									const downloadTask = uni.downloadFile({
+										url: res.url,
+										success: (res) => {
+											if (res.statusCode === 200) {
+												plus.nativeUI.closeWaiting()
+												plus.runtime.install(res.tempFilePath, {
+													force: false
+												}, (e) => {
+													uni.showToast({
+														title: '更新成功',
+														icon: 'none',
+														duration: 1000
+													})
+													setTimeout(() => {
+														plus.runtime.restart()
+													},1000)
+												}, (e) => {
+													uni.showToast({
+														title: e.message,
+														icon: 'none'
+													})
+												})
+											}
+										}
+									})
+									plus.nativeUI.showWaiting()
+								}
+							}
+						})
+					}
+				})
+			});
+			// #endif
 		},
-		onShow: function() {
-		},
-		onHide: function() {
-		}
+		onShow: function() {},
+		onHide: function() {}
 	}
 </script>
 
@@ -38,9 +87,10 @@
 	/* #ifndef APP-PLUS-NVUE */
 	@import "colorui/main.css";
 	@import "colorui/icon.css";
-	
+
 	.cu-form-group .title {
 		min-width: calc(4em + 15px);
 	}
+
 	/* #endif */
 </style>
